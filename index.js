358b3b02d5b93339e2b1da7ae6d0384909aed409
Arthur Violy <arthur@violy.net>
2017-02-15 22:10:32 +0100

    - host base cdn - tile server cache improvements

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
+var imageSize = require('image-size');
 var Canvas = require('canvas'),
     Image = Canvas.Image;
 var app = express();
 var port = 3333;
 
-var TILE_SIZE = 256;
+var TILE_SIZE = 256,
+    cacheRoot = '.cache/',
+    emptyTileSrc = 'empty.png';
 
 var imageOriginSrc, // référence unique
     imageOrigin; // référence unique utilisée comme pseudo-cache
 
 app.use(express.static('public'));
+
+app.use('/tile/empty.png',express.static(__dirname+'/'+cacheRoot+emptyTileSrc));
 
 app.get('/tile/:z/:x/:y', function (req, res) {
     console.log(req.params);
@@ -22,67 +27,74 @@ app.get('/tile/:z/:x/:y', function (req, res) {
     var fileName = 'Waldseemuller_map_2.jpg',
         srcPath = 'images/',
         srcFilePath = srcPath+fileName,
-        cachePath = '.cache/' + z + '/' + x + '/' + y + '/',
-        cacheFilePath = cachePath+fileName;
+        cachePath = cacheRoot + z + '/' + x + '/' + y + '/',
+        cacheFilePath = cachePath+fileName,
+        srcSize = imageSize(srcFilePath),
+        maxZoom = Math.round(Math.max(Math.sqrt(srcSize.width/TILE_SIZE), Math.sqrt(srcSize.height/TILE_SIZE))-1),
+        scale = Math.pow(2,-z),
+        centerX = srcSize.width/2,
+        centerY = srcSize.height/2,
+        scaledTile = scale * TILE_SIZE,
+        offsetX = (x) * scaledTile + centerX,
+        offsetY = (y) * scaledTile + centerY,
+        contain = offsetX > -scaledTile && offsetX<srcSize.width && offsetY > -scaledTile && offsetY<srcSize.height;
 
-    function EndImage(buf){
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': buf.length
+    console.log(offsetX > -scaledTile , offsetX<srcSize.width , offsetY > -scaledTile , offsetY<srcSize.height);
+
+    function EndImage(canvas,filePath,dirPath){
+        canvas.toBuffer(function (err, buf) {
+            if (err) throw err;
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': buf.length
+            });
+            res.end(buf);
+            // écrit le fichier en local
+            console.log('write file %s ...', filePath);
+            mkdirp(dirPath, function (err) {
+                if (err) throw err;
+                console.log('directory %s is Ok.', dirPath);
+                fs.writeFile(filePath, buf, function (err) {
+                    if (err) throw err;
+                    console.log('cache file done. %s', filePath);
+                });
+            })
         });
-        res.end(buf);
+
     }
 
-    fs.exists(cacheFilePath, function (exists) {
-        if (exists) {
-            console.log('file exists', cacheFilePath);
-            fs.readFile(cacheFilePath,function(err,buf){
-                if(err) throw err;
-                EndImage(buf);
-            });
-        } else {
-            console.log('cache not exists', cacheFilePath);
-            var canvas = new Canvas(TILE_SIZE, TILE_SIZE),
-                ctx = canvas.getContext('2d'),
-                imgSrc,
-                img = new Image();
+    if(contain) {
+        res.sendFile(cacheFilePath, {maxAge: '1d', root: __dirname}, function (err) {
+            if (err) {
+                console.log('cache not exists', cacheFilePath);
+                var canvas = new Canvas(TILE_SIZE, TILE_SIZE),
+                    ctx = canvas.getContext('2d'),
+                    imgSrc,
+                    img = new Image();
 
-            if (imageOriginSrc != fileName) {
-                console.log('imageOrigin source --> %s', fileName);
-                imageOriginSrc = fileName;
-                imageOrigin = fs.readFileSync(srcFilePath);
-            }
-            imgSrc = imageOrigin;
+                if (imageOriginSrc != fileName) {
+                    console.log('imageOrigin source --> %s', fileName);
+                    imageOriginSrc = fileName;
+                    imageOrigin = fs.readFileSync(srcFilePath);
+                }
+                imgSrc = imageOrigin;
 
-            img.src = imgSrc;
-            var maxZoom = Math.round(Math.max(Math.sqrt(img.width/TILE_SIZE), Math.sqrt(img.height/TILE_SIZE))-1),
-                scale = Math.pow(2,maxZoom-z),
-                centerOffsetX = img.width/2,
-                centerOffsetY = img.height/2;
+                img.src = imgSrc;
 
-            console.log('x %s y %s z %s scale %s maxZoom %s', x, y, z, scale, maxZoom);
+                console.log('x %s y %s z %s scale %s maxZoom %s', x, y, z, scale, maxZoom);
                 console.log('Tile ', fileName, img.width, img.height);
                 // now, lets draw the tile
-                ctx.drawImage(img, ((x-.5) * scale * TILE_SIZE) + centerOffsetX, ((y-.5) * scale * TILE_SIZE) + centerOffsetY, TILE_SIZE*scale, TILE_SIZE*scale,0,0, TILE_SIZE, TILE_SIZE);
+                ctx.drawImage(img, offsetX, offsetY, TILE_SIZE * scale, TILE_SIZE * scale, 0, 0, TILE_SIZE, TILE_SIZE);
                 // and transform it into a binary buffer, so we can
                 // deliver it to the client
-                canvas.toBuffer(function (err, buf) {
-                    if (err) throw err;
-                    EndImage(buf);
-                    // écrit le fichier en local
-                    console.log('write file %s ...',cacheFilePath);
-                    mkdirp(cachePath,function(err){
-                        if(err) throw err;
-                        console.log('directory %s is Ok.',cachePath);
-                        fs.writeFile(cacheFilePath, buf, function (err) {
-                            if (err) throw err;
-                            console.log('cache builded');
-                        });
-                    })
-                });
-
-        }
-    });
+                EndImage(canvas,cacheFilePath,cachePath);
+            }
+        });
+    }else{
+        res.sendFile(cacheRoot+emptyTileSrc,{maxAge: '1d', root: __dirname},function(err){
+            if (err) throw err;
+        });
+    }
 
 });
 
--- public/js/main.js
+++ public/js/main.js
@@ -5,22 +5,37 @@ $(document).ready(function(){
         CRSWidth = 1000,
         CRSHeight = 1000,
         offsetX = (baseWidth-CRSWidth)/2,
-        offsetY = (baseHeight-CRSHeight)/2;
+        offsetY = (baseHeight-CRSHeight)/2,
+        host = window.location.host;
 
 
     var bounds = [[0,0],[baseHeight,baseWidth]];
 
+    var tileLayer = L.tileLayer('http://{s}.cdn.'+host+'/tile/{z}/{x}/{y}',
+        {
+            foo:'bar',
+            subdomains:'abcdef',
+            minZoom:-6,
+            maxZoom:0,
+            maxNativeZoom:0,
+            minNativeZoom:-6
+        }
+    );
+
     var map = L.map('map', {
         attributionControl:false, // cache l’attribution Leaflet
         crs: L.CRS.Simple, // Coordinate Reference System
-        maxBounds:bounds,
-        minZoom:-5
+        //maxBounds:bounds,
+        minZoom:-5,
+        maxZoom:0,
+        layers:[tileLayer],
     });
+
+    map.fitBounds([[0,0],[1000,1000]]);
 
 
     //
     // 13708,7590
     //
-    var image = L.imageOverlay('img/Waldseemuller_map_2.jpg', bounds).addTo(map);
-    map.fitBounds(bounds);
+
 })
